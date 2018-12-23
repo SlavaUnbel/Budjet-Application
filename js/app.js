67138d11a0d95257b534ea61cd2f40ef19c171e1
Unbelievable <slavlen1999@gmail.com>
2018-12-23 20:12:11 +0200

    finish with addExpense and deleteExpense methods

--- js/app.js
+++ js/app.js
@@ -104,11 +104,37 @@ class UI {
         this.expenseAmount.textContent = total;
         return total;
     }
+    //edit expense method
     editExpense(element) {
-
+        let id = parseInt(element.dataset.id);
+        let parent = element.parentElement.parentElement.parentElement;
+        //remove from dom
+        this.expenseList.removeChild(parent);
+        //remove from the list
+        let expense = this.itemList.filter(function(item) {
+            return item.id === id;
+        });
+        //show value
+        this.expenseInput.value = expense[0].title;
+        this.amountInput.value = expense[0].amount;
+        let tempList = this.itemList.filter(function(item) {
+            return item.id !== id;
+        });
+        this.itemList = tempList;
+        this.showBalance();
     }
+    //delete expense method
     deleteExpense(element) {
-        
+        let id = parseInt(element.dataset.id);
+        let parent = element.parentElement.parentElement.parentElement;
+        //remove from dom
+        this.expenseList.removeChild(parent);
+        //remove from the list
+        let tempList = this.itemList.filter(function(item) {
+            return item.id !== id;
+        });
+        this.itemList = tempList;
+        this.showBalance();
     }
 }
 
