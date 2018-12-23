131a1fddbe3fc3ead647aa3453f403fe4e5f37d2
Unbelievable <slavlen1999@gmail.com>
2018-12-23 19:21:28 +0200

    finish with method submitBudgetForm and correct spelling mistake causing the mistake

--- js/app.js
+++ js/app.js
@@ -15,27 +15,38 @@ class UI {
         this.itemList = [];
         this.itemID = 0;
     }
-    //submit budjet method
-    submitBudjetForm() {
-        const value = this.budjetInput.value;
+    //submit budget method
+    submitBudgetForm() {
+        const value = this.budgetInput.value;
         if(value === '' || value < 0) {
-            this.budjetFeedback.classList.add('showItem');
-            this.budjetFeedback.innerHTML = `<p>value cannot be empty or negative</p>`;
+            this.budgetFeedback.classList.add('showItem');
+            this.budgetFeedback.innerHTML = `<p>value cannot be empty or negative</p>`;
+            const self = this;
+            console.log(this);
+            setTimeout(function() {
+                self.budgetFeedback.classList.remove('showItem');
+            }, 4000);
+        }
+        else {
+            this.budgetAmount.textContent = value;
+            this.budgetInput.value = '';
+            this.showBalance();
         }
     }
+    
 }
 
 function eventListeners() {
-    const budgetForm = document.getElementById('budjet-form');
+    const budgetForm = document.getElementById('budget-form');
     const expenseForm = document.getElementById('expense-form');
     const expenseList = document.getElementById('expense-list');
 
     //new instance of UI class
     const ui = new UI();
-    //budjet form submit
-    budjetForm.addEventListener('submit', function(event) {
+    //budget form submit
+    budgetForm.addEventListener('submit', function(event) {
         event.preventDefault();
-        ui.submitBudjetForm();
+        ui.submitBudgetForm();
     });
     //expense form submit
     expenseForm.addEventListener('submit', function(event) {
